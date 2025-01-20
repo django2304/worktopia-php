4ac6e9a80eb0a1c4ac24ec5dffe87a3c108e37e4
django2304 <chichilinskiy@gmail.com>
2025-01-20 10:23:55 +0200

    Inspect Helper Functions

--- helpers.php
+++ helpers.php
@@ -20,6 +20,7 @@ function basePath($path = '')
 function loadView($name)
 {
     $viewPath = basePath("views/{$name}.view.php");
+
     if (file_exists($viewPath)) {
         require $viewPath;
     } else {
@@ -42,4 +43,30 @@ function loadPartial($name)
     } else {
         echo "Partial '{$name} not found!'" . '<br>';
     }
+}
+
+/**
+ * Inspect value(s)
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspect($value)
+{
+    echo '<pre>';
+    var_dump($value);
+    echo '<pre>';
+}
+
+/**
+ * Inspect value(s) and die
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value)
+{
+    echo '<pre>';
+    die(var_dump($value));
+    echo '<pre>';
 }
