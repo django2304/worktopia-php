358fbd1525541d57d0f83ec04c16f7df76f7fff3
django2304 <chichilinskiy@gmail.com>
2025-01-25 10:47:08 +0200

    Pass the data to view and change hardcode to dynamic

--- controllers/home.php
+++ controllers/home.php
@@ -5,6 +5,7 @@ $db = new Database($config);
 
 $listings = $db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
 
-inspect($listings);
 
-loadView('home');
+loadView('home', [
+    'listings' => $listings
+]);
