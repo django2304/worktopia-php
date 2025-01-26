11c63170fa36e15299c60503eaf921dfe5c604c1
django2304 <chichilinskiy@gmail.com>
2025-01-27 01:34:32 +0200

    Refactor routes and controllers for new type

--- App/controllers/HomeController.php
+++ App/controllers/HomeController.php
@@ -1,10 +1,25 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controllers;
+
+use Framework\Database;
 
 class HomeController
 {
-    public function __construct() {}
+    protected $db;
 
-    public function index() {}
+    public function __construct()
+    {
+        $config = require basePath('config/db.php');
+        $this->db = new Database($config);
+    }
+
+    public function index()
+    {
+        $listings = $this->db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
+
+        loadView('home', [
+            'listings' => $listings
+        ]);
+    }
 }
